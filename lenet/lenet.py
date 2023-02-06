ab4f2f617cc7d8d0b6b941f817e598ca3f4300eb
toto_wng <r.news94@gmail.com>
2023-02-06 11:22:25 +0100

    cleanup

--- lenet/lenet.py
+++ lenet/lenet.py
@@ -7,14 +7,14 @@ from torchinfo import summary
 
 class LeNet(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.relu = nn.ReLU()
-        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=0)
-        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0)
-        self.conv3 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1, padding=0)
-        self.linear1 = nn.Linear(120, 84)
-        self.linear2 = nn.Linear(84, 10)
+      super().__init__()
+      self.relu = nn.ReLU()
+      self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
+      self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=0)
+      self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0)
+      self.conv3 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1, padding=0)
+      self.linear1 = nn.Linear(120, 84)
+      self.linear2 = nn.Linear(84, 10)
 
     def forward(self, x):
       # conv1
